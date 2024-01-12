9e0199cad307679d31b1bd5940c32539c98fcb7a
shirokuma89dev <code@shirokuma89.dev>
2024-01-12 10:11:56 +0900

    ✨ 送信側

--- XIAOC3-Sender/src/main.cpp
+++ XIAOC3-Sender/src/main.cpp
@@ -1,18 +1,99 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+BLEServer* pServer = NULL;
+BLECharacteristic* pCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
+
+#define SERVICE_UUID "fe77e1f2-1e06-11ee-be56-0242ac120002"
+#define CHARACTERISTIC_UUID "060254ca-1e07-11ee-be56-0242ac120002"
+
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+        deviceConnected = true;
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+        deviceConnected = false;
+    }
+};
+
+class MyCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pCharacteristic) {
+        std::string rxValue = pCharacteristic->getValue();
+
+        if (rxValue.length() > 0) {
+            for (int i = 0; i < rxValue.length(); i++)
+                Serial.println(rxValue[i]);
+        }
+    }
+};
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+
+    // Create the BLE Device
+    BLEDevice::init("DEV1");
+
+    // Create the BLE Server
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    // Create the BLE Service
+    BLEService* pService = pServer->createService(SERVICE_UUID);
+
+    pCharacteristic = pService->createCharacteristic(
+        CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ |
+                                 BLECharacteristic::PROPERTY_WRITE |
+                                 BLECharacteristic::PROPERTY_NOTIFY |
+                                 BLECharacteristic::PROPERTY_INDICATE);
+
+    pCharacteristic->addDescriptor(new BLE2902());
+    pCharacteristic->setCallbacks(new MyCallbacks());
+
+    pService->start();
+
+    BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVICE_UUID);
+    pAdvertising->setScanResponse(false);
+    pAdvertising->setMinPreferred(0x0);
+    BLEDevice::startAdvertising();
+    Serial.println("Waiting a client connection to notify...");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    if (deviceConnected) {
+        if (Serial.available() != 0) {
+            int dataSize = 0;
+            uint8_t sendDataArr[140] = {0};
+            while (Serial.available() != 0) {
+                uint8_t data = Serial.read();
+                sendDataArr[dataSize] = data;
+                dataSize++;
+            }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+            pCharacteristic->setValue(sendDataArr, dataSize);
+            pCharacteristic->notify();
+        }
+
+        delay(2);
+    } else {
+        Serial.println("Waiting a client connection to notify...");
+    }
+
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(100);
+        pServer->startAdvertising();
+        oldDeviceConnected = deviceConnected;
+    }
+    if (deviceConnected && !oldDeviceConnected) {
+        Serial.println("Connected");
+        oldDeviceConnected = deviceConnected;
+    }
 }
